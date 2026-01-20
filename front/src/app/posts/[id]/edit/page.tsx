6d936a45095a2759d0085a64e0461da5f83772bb
jhs512 <jangka512@gmail.com>
2026-01-20 17:44:44 +0900

    0077

--- front/src/app/posts/[id]/edit/page.tsx
+++ front/src/app/posts/[id]/edit/page.tsx
@@ -5,21 +5,19 @@ import { useRouter } from "next/navigation";
 import { use } from "react";
 
 import usePost from "@/domain/post/hooks/usePost";
-import { useAuthContext } from "@/global/auth/hooks/useAuth";
+import withLogin from "@/global/auth/hoc/withLogin";
 
-export default function Page({ params }: { params: Promise<{ id: string }> }) {
+export default withLogin(function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const router = useRouter();
 
   const { id: idStr } = use(params);
   const id = parseInt(idStr);
 
   const { post, modifyPost } = usePost(id);
-
-  const { isLogin } = useAuthContext();
-
-  if (!isLogin) {
-    return <div>로그인 후 이용해주세요.</div>;
-  }
 
   if (post == null) return <div>로딩중...</div>;
 
@@ -80,4 +78,4 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
       </form>
     </>
   );
-}
+});
